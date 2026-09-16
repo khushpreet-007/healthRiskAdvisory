cef1d047cc1b2b6aa18f50a67d813898e73cbded
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2026-09-16 00:21:47 +0000

    mark 4

--- app/static/firebase-messaging-sw.js
+++ app/static/firebase-messaging-sw.js
@@ -1,50 +1,50 @@
-console.log("Firebase service worker loaded");
+// console.log("Firebase service worker loaded");
 
-self.addEventListener("install", () => {
-    console.log("Firebase service worker installed");
-});
-
-self.addEventListener("activate", () => {
-    console.log("Firebase service worker activated");
-});
-
-// importScripts(
-//     'https://www.gstatic.com/firebasejs/12.16.0/firebase-app-compat.js'
-// );
-
-// importScripts(
-//     'https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging-compat.js'
-// );
-
-
-
-// firebase.initializeApp({
-//     apiKey: "AIzaSyDC8TBV5wqf2DX9jRblHmZEnv69l6IPr3c",
-//     authDomain: "imageedit-498605.firebaseapp.com",
-//     projectId: "imageedit-498605",
-//     appId: "1:931055668161:web:3a68b7b7f2e63a6b357d40"
+// self.addEventListener("install", () => {
+//     console.log("Firebase service worker installed");
 // });
 
+// self.addEventListener("activate", () => {
+//     console.log("Firebase service worker activated");
+// });
 
-// const messaging = firebase.messaging();
+importScripts(
+    'https://www.gstatic.com/firebasejs/12.16.0/firebase-app-compat.js'
+);
 
-// messaging.onBackgroundMessage(
-//     (payload) => {
-
-
-//         self.registration.showNotification(
-
-//             payload.notification.title,
-
-//             {
-
-//                 body:
-//                     payload.notification.body
-
-//             }
-
-//         );
+importScripts(
+    'https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging-compat.js'
+);
 
 
-//     });
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDC8TBV5wqf2DX9jRblHmZEnv69l6IPr3c",
+    authDomain: "imageedit-498605.firebaseapp.com",
+    projectId: "imageedit-498605",
+    appId: "1:931055668161:web:3a68b7b7f2e63a6b357d40"
+});
+
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(
+    (payload) => {
+
+
+        self.registration.showNotification(
+
+            payload.notification.title,
+
+            {
+
+                body:
+                    payload.notification.body
+
+            }
+
+        );
+
+
+    });
 
